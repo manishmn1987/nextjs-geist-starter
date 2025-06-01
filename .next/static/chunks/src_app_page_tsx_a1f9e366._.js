3dd08b5408da7e1cb17585fb11184ed8094d3f40
manishmn1987 <40922479+manishmn1987@users.noreply.github.com>
2025-06-02 03:54:21 +0530

    Add .next/static/chunks/src_app_page_tsx_a1f9e366._.js 1748816660471

---
+++ .next/static/chunks/src_app_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3b196ccc._.js",
+  "static/chunks/src_9a34df08._.js"
+],
+    source: "dynamic"
+});
